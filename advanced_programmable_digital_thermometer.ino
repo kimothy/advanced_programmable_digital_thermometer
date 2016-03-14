58c8813101134569c2a5af22da667a6af3e6ae63
Kim Timothy Engh <kte@kte-online.net>
2016-03-14 23:36:53 +0100

    new design function. test failed. good night

--- advanced_programmable_digital_thermometer.ino
+++ advanced_programmable_digital_thermometer.ino
@@ -26,62 +26,44 @@ void loop() {
   delay(1000);
 }
 
-void setDigit(signed int tempDigit){
- int i = 0;
- int j = 0;
+void setDigit(signed int temp){
+ unsigned int i = 0;
+ unsigned int j = 0;
  
- signed int static firstDigit = 0;
- signed static secondDigit = 0;
- 
- signed int static firstDigitPrevious = 0;
- signed int static secondDigitPrevious = 0;
- 
- const int digits[11][13] = {{0,1,2,10,18,26,34,33,32,24,16,8,NULL},
-                             {0,1,9,17,25,32,33,34,NULL,NULL,NULL,NULL,NULL},
-                             {0,1,2,10,18,17,16,24,32,33,34,NULL,NULL},
-                             {0,1,2,10,18,26,34,33,32,17,NULL,NULL,NULL},
-                             {0,8,16,17,2,10,18,26,34,NULL,NULL,NULL,NULL},
-                             {2,1,0,8,16,17,18,26,34,33,32,NULL,NULL},
-                             {2,1,0,8,16,24,32,33,34,26,18,17,NULL},
-                             {0,1,2,10,17,25,33,NULL,NULL,NULL,NULL,NULL,NULL},
+ const int digits[11][13] = {{0,1,2,10,18,26,34,33,32,24,16,8,-1},
+                             {0,1,9,17,25,32,33,34,-1,-1,-1,-1,-1},
+                             {0,1,2,10,18,17,16,24,32,33,34,-1,-1},
+                             {0,1,2,10,18,26,34,33,32,17,-1,-1,-1},
+                             {0,8,16,17,2,10,18,26,34,-1,-1,-1,-1},
+                             {2,1,0,8,16,17,18,26,34,33,32,-1,-1},
+                             {2,1,0,8,16,24,32,33,34,26,18,17,-1},
+                             {0,1,2,10,17,25,33,-1,-1,-1,-1,-1,-1},
                              {0,1,2,10,18,17,16,24,32,33,34,26,8},
-                             {17,16,8,0,1,2,10,18,26,34,NULL,NULL,NULL},
-                             {NULL,16,17,18,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL}};//negativ
+                             {17,16,8,0,1,2,10,18,26,34,-1,-1,-1},
+                             {-1,16,17,18,-1,-1,-1,-1,-1,-1,-1,-1,-1}};
   
-  tempDigit = constrain(tempDigit, -9, 99);
+  unsigned int displayMatrixNext[40];
+  unsigned int tempDigit[2];
+
+  temp = constrain(temp, -9, 99);
+  tempDigit[0] = abs(temp / 10 % 10);
+  tempDigit[1] = abs(temp % 10);
   
-  firstDigit = abs(tempDigit / 10 % 10);
-  secondDigit = abs(tempDigit % 10);
-  
-  if ((firstDigitPrevious != firstDigit) or (secondDigitPrevious != secondDigit))
-  {
-    firstDigitPrevious = firstDigit;
-    secondDigitPrevious = secondDigit;
-    
-    setBackroundColor(backgroundColor);
-    for (i = 0; i<13;i++)
-    { 
-      if (tempDigit > 9){ // skip first digit if less then 10
-      strip.setPixelColor(digits[firstDigit][i],0,120,20);
-    }
-    else if ((tempDigit < 0) && (digits[10][i] != NULL)) { //if negative numbers
-      strip.setPixelColor(digits[10][i],0,120,20);
-    }
-    
-    strip.setPixelColor(digits[secondDigit][i]+4,0,120,20);
-    
-    strip.show();
+  for (i = 0; i < 40; i++){
+    for (j = 0; i < 13; j++){
+      if (i == digits[tempDigit[0]][j]){
+        displayMatrixNext[i] = 1;
+      } 
+      if (i == digits[tempDigit[1]][j]){
+        displayMatrixNext[i+5] = 1;
+      }
     }
   }
-}
 
-
-void setBackroundColor(int* rgb)
-{
-  int i = 0;
-  for (i = 0; i<40; i++)
-  {
-    strip.setPixelColor(i,rgb[0],rgb[1],rgb[2]);
+  for (i = 0; i < 40; i++){
+    if (displayMatrixNext[i] == 1){
+            strip.setPixelColor(displayMatrixNext[i],0,120,20);
+    }
   }
-  strip.show();  
+  strip.show();
 }
